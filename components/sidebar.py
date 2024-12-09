b8f136718690496b7d1da4d2b762ad1e53c49bdb
Kevin Zyskowski <zyskowski.k@northeastern.edu>
2024-12-08 22:18:53 -0500

    Delete components directory

--- components/sidebar.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def sidebar_menu():
-    with st.sidebar:
-        st.header("User Menu")
-        st.button("Log In", key="login_sidebar")
-        st.button("Account Settings", key="account_settings_sidebar")
-        st.button("Help", key="help_sidebar")
